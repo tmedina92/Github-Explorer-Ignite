3701814e3f928b722737c337e2477d55adc37b81
thamis medina <thamismedina@gmail.com>
2022-05-17 17:39:20 -0300

    add a source map

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,4 @@
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 import { App } from './App';
 
-render( <App />, document.getElementById('root') )
+root.render( <App />, document.getElementById('root') )
